961d05a9ee2d91f716de5f3a37dc04c531d126bd
Kartik Singhal <kartiksinghal@gmail.com>
2012-01-11 13:27:42 +0530

    removed non working links

--- lab2-jan10/3.sh
+++ lab2-jan10/3.sh
@@ -4,6 +4,8 @@
 # ending up with a prompt with your name and you will be able to execute all
 # scripts (questions given for lab assignmnet 1 and 2)with short commands.
 
+export PATH=$PATH:`pwd`/../lab1-jan4
+echo $PATH
 while :
 do
 	echo -ne "kartik > "

31a976c34899566e926e655c7b6e78d99466fc81
Kartik Singhal <kartiksinghal@gmail.com>
2012-01-22 19:48:55 +0530

    improved cp to satisfaction, added variable arg implementation of error

--- lab3-jan23/ass1.c
+++ lab3-jan23/ass1.c
@@ -1,22 +1,31 @@
+/*
+ * 1. Write a C program to implement 'cp' command in linux.
+ * (Source and destination files should be taken from command line)
+ */
+
 #include <unistd.h>	//read, write
-#include <fcntl.h>	//open
+#include <fcntl.h>	//open, creat
 #include "syscalls.h"	//error
+
+#define BUFSIZE 1000
+#define PERMS 0600
 
 int main(int argc, char *argv[])
 {
 	if (argc != 3)
 		error("Usage: cp source destination");
 
-	char buf[1024];
-	int sourcefd, destfd;
-	int count;
+	char buf[BUFSIZE];
+	int sourcefd, destfd, count;
 
-	if ((sourcefd = open(argv[1], O_RDONLY)) < 0)
-		error("open failed for specified source file.");
-	destfd = open(argv[2], O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
+	if ((sourcefd = open(argv[1], O_RDONLY)) == -1)
+		error("cp: failed to open %s", argv[1]);
+	if ((destfd = creat(argv[2], PERMS)) == -1)
+		error("cp: failed to open %s for writing", argv[2]);
 
-	while ((count = read(sourcefd, buf, sizeof(buf))) > 0)
-		write(destfd, buf, count);
+	while ((count = read(sourcefd, buf, BUFSIZE)) > 0)
+		if ((write(destfd, buf, count)) != count)
+			error("cp: write error on file %s", argv[2]);
 
 	return 0;
 }
--- lab3-jan23/syscalls.h
+++ lab3-jan23/syscalls.h
@@ -1,8 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
 
-void error(char *message)
+void error(char *message, ...)
 {
-	printf("Error: %s\n", message);
-	exit(EXIT_FAILURE);
+	va_list args;
+
+	va_start(args, message);
+	fprintf(stderr, "error: ");
+	vfprintf(stderr, message, args);
+	fprintf(stderr, "\n");
+	va_end(args);
+	exit(1);
 }

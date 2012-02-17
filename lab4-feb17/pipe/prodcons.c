43b118eb6163322402d9a8dfc094f63e9f406e9d
Kartik Singhal <kartiksinghal@gmail.com>
2012-02-17 13:11:35 +0530

    pipe implementation done - not satisfied

---
+++ lab4-feb17/pipe/prodcons.c
@@ -0,0 +1,44 @@
+#include "shared.h"
+
+int produce_item() {
+        return 1;
+}
+
+void consume_item(int item) {
+        ;
+}
+
+int main()
+{
+        int item = 0;
+        int pipefd[2];
+	pid_t pid;
+
+	if (pipe(pipefd) == -1)
+	{
+                printf("pipe: error creating pipe");
+		exit(-1);
+	}
+	
+	pid = fork();
+	
+	if (pid == 0) { //consumer
+                close(pipefd[1]);
+                while(1) {
+                        read(pipefd[0], &item, sizeof(item));
+                        
+                        printf("consumer: %d\n", item);
+                        consume_item(item);
+                }
+        }
+        else { //producer
+                close(pipefd[0]);
+                while(1) {
+                        item = produce_item();                      
+                        write(pipefd[1], &item, sizeof(item));
+                        printf("producer: %d\n", item);
+                }
+        }
+
+        return 0;
+}
---
+++ lab4-feb17/pipe/shared.h
@@ -0,0 +1,9 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#define BUFFER_SIZE 10
+
+void clear_buffer(int *sbuf);
+void print_buffer(int *sbuff);
